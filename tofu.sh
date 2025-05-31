ca3c299124c718efef5ea7b3f68ae95d345e1519
oysvendsen <oyvind.svendsen1509@gmail.com>
2025-06-01 01:51:20 +0200

    Add -auto approve flag to destroy shortcut so the shortcut/script works for destroying tofu resources.

--- tofu.sh
+++ tofu.sh
@@ -74,7 +74,7 @@ function destroy() {
         --workdir=/srv/workspace \
         --mount type=bind,source=.,target=/srv/workspace \
         ghcr.io/opentofu/opentofu:latest \
-        destroy
+        destroy -auto-approve
 }
 
 function kubeconfig() {
